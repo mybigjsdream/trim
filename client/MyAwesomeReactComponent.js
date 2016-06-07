9a471eb273201982bfbd6e5dc70046c1592e54c4
shanyu-e <554816284@qq.com>
2016-06-07 17:11:15 +0800

    init--使用mantra  之前的  不可用原因

---
+++ client/MyAwesomeReactComponent.js
@@ -0,0 +1,12 @@
+/**
+ * Created by dengjing on 16/6/7.
+ */
+
+import React from 'react';
+import RaisedButton from 'material-ui/RaisedButton';
+
+const MyAwesomeReactComponent = () => (
+    <RaisedButton label="Default" />
+);
+
+export default MyAwesomeReactComponent;
