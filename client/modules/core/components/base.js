df5528085cfb0cc6838a26acda755d79fe83a856
shanyu-e <554816284@qq.com>
2016-06-12 18:27:04 +0800

    可以css控制 material

--- client/modules/core/components/base.js
+++ client/modules/core/components/base.js
@@ -1,6 +1,10 @@
 import React from 'react';
 
 import AppBar from 'material-ui/AppBar';
+import IconButton from 'material-ui/IconButton';
+import IconMenu from 'material-ui/IconMenu';
+import MenuItem from 'material-ui/MenuItem';
+import MoreVertIcon from 'material-ui/svg-icons/navigation/more-vert';
 
 import baseTheme from 'material-ui/styles/baseThemes/lightBaseTheme';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
@@ -13,11 +17,25 @@ class Base extends React.Component {
     }
 
     render() {
+        const style = {
+            position: "fixed",
+            top: 0,
+            left: 0,
+        };
         return (
-            <div>
-                <div>deng</div>
-                <AppBar title="Title"/>
-            </div>
+                <AppBar
+                    title="应用"
+                    style={style}
+                    iconElementRight={
+                      <IconMenu
+                          iconButtonElement={<IconButton><MoreVertIcon /></IconButton>}
+                          targetOrigin={{horizontal: 'right', vertical: 'top'}}
+                          anchorOrigin={{horizontal: 'right', vertical: 'top'}}>
+                          <MenuItem primaryText="Refresh" />
+                          <MenuItem primaryText="Help" />
+                          <MenuItem primaryText="Sign out" />
+                      </IconMenu>
+                    }/>
         );
     }
 
